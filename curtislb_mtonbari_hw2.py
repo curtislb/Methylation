38150adaa4deb8df3e5df31ee91dc1ce42239dae
Curtis Belmonte <curtislb@princeton.edu>
2015-03-30 19:02:31 -0400

    Adds python script

--- curtislb_mtonbari_hw2.py
+++ curtislb_mtonbari_hw2.py
@@ -2,8 +2,9 @@ import numpy as np
 
 def beta_from_train(fname):
     with open(fname) as f:
-        X = np.array()
+        X = np.array([])
         for line in f:
+            print 'hi'
             beta = line.rstrip().split()[4:-1]
             X = np.vstack(X, beta)
         return X
@@ -15,5 +16,5 @@ def test_model(model):
     pass
 
 if __name__ == '__main__':
-    X = beta_from_train('intersected_final_chr1_cutoff_20_train_revised.bed')
+    X = beta_from_train('data/intersected_final_chr1_cutoff_20_train_revised.bed')
     print X
