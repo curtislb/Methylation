0bfb67136623d539a87e6e433df9ec6603053ea6
Curtis Belmonte <curtislb@princeton.edu>
2015-03-30 20:03:28 -0400

    Adds naive mean imputation and code for processing train data

--- curtislb_mtonbari_hw2.py
+++ curtislb_mtonbari_hw2.py
@@ -1,12 +1,54 @@
+import math
 import numpy as np
 
-def beta_from_train(fname):
+N_FEATURES = 33
+CHR1_LINES = 379551
+
+def rmse(y_pred, y_true):
+    sse = 0.0
+    n = 0
+    for i in xrange(len(y_pred)):
+        if not math.isnan(y_true[i]):
+            err = y_pred[i] - y_true[i]
+            sse += err * err
+            n += 1
+    return math.sqrt(sse / n)
+
+def naive_mean_impute(fname, fsize):
     with open(fname) as f:
-        X = np.array([])
+        y = np.empty(fsize, float)
+        i = 0
         for line in f:
-            print 'hi'
-            beta = line.rstrip().split()[4:-1]
-            X = np.vstack(X, beta)
+            beta = [float(b) for b in line.rstrip().split()[4:-1]]
+            b_sum = 0.0
+            b_count = 0
+            for b in beta:
+                if not math.isnan(b):
+                    b_sum += b
+                    b_count += 1
+            y[i] = b_sum / float(b_count)
+            i += 1
+        return y
+    
+def test_vals_from_file(fname, fsize):
+    with open(fname) as f:
+        y = np.empty(fsize, float)
+        i = 0
+        for line in f:
+            y[i] = float(line.rstrip().split()[-2])
+            i += 1
+        return y
+    
+def beta_from_train(fname, fsize):
+    with open(fname) as f:
+        X = np.empty((fsize, N_FEATURES), float)
+        i = 0
+        for line in f:
+            if i % 1000 == 0:
+                print i
+            beta = [float(b) for b in line.rstrip().split()[4:-1]]
+            X[i,:] = np.array(beta)
+            i += 1
         return X
 
 def train_model(model):
@@ -16,5 +58,7 @@ def test_model(model):
     pass
 
 if __name__ == '__main__':
-    X = beta_from_train('data/intersected_final_chr1_cutoff_20_train_revised.bed')
-    print X
+#     y_pred = naive_mean_impute('data/intersected_final_chr1_cutoff_20_train_revised.bed', CHR1_LINES)
+#     y_true = test_vals_from_file('data/intersected_final_chr1_cutoff_20_test.bed', CHR1_LINES)
+#     print rmse(y_pred, y_true)
+    X = beta_from_train('data/intersected_final_chr1_cutoff_20_train_revised.bed', CHR1_LINES)
